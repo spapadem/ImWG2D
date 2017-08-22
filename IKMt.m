508c8feb71db65b6c4bbf71d2419d0499ff3283e
Symeon Papadimitropoulos <simpapadem@gmail.com>
2017-08-22 12:41:56 +0300

    Update IKMt.m

--- IKMt.m
+++ IKMt.m
@@ -3,20 +3,38 @@ close all;
 %
 % Projected Kirchhoff migration for a homogeneous 2D waveguide with 
 % Dirichlet/Dirichlet boundary conditions.
-%
+% 
+% The method was introduced in [1] for the case where the array spans the
+% whole depth of the waveguide.
+% 
+% [1] C. Tsogka, D. A. Mitsoudis, and S. Papadimitropoulos. 
+% Selective imaging of extended reflectors in two-dimensional waveguides. 
+% SIAM J. Imaging Sci., 6(4):2714{2739, 2013.
+% 
 % Call as :
 %
 % [IKMT,z1,x1] = IKMt(frqs,J)
 %
 %
-% frqs   : the frequency (or frequencies) we use in the computation
-%          (enter an array for multiple frequencies).
+% frqs: the frequency (or frequencies) we use in the computation
+%       (enter an array for multiple frequencies).
 %
-% J      : selective imaging using subspace projection on the Jth singular 
-%          vector.
-%          Enter 0 or leave blank for no projection,
-%          Enter a vector if you want to project on more than one singular 
-%          vector.
+% J   : selective imaging using subspace projection on the Jth singular 
+%       vector.
+%       Enter 0 or leave blank for no projection,
+%       Enter a vector if you want to project on more than one singular 
+%       vector.
+% 
+% Examples of use:
+%       [IKMT,z1,x1] = IKMt(73); Uses a single frequency and no projection.
+%       [IKMt,z1,x2] = IKMt(31.875:3.75:118.125); Uses multiple frequencies
+%                      and no projection.
+%       [IKMt,z1,x1] = IKMt(73,1); Uses a single frequency and projection
+%                       on the first singular vector.
+%       [IKMt,z1,x1] = IKMt(73,[1,3,5]); Uses a single frequency and 
+%                      projection on the first, third and fifth singular 
+%                      vectors (the result of each projection is summed.
+
 
 tt= tic;
 
